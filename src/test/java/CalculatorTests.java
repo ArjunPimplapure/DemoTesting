4016903f0523a0e7123edf865f0f3cd5d1bf8b46
ArjunPimplapure <arjunpimplapure1@gmail.com>
2023-08-01 16:06:31 +0530

    First Commit

---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import app.Calculator;
+
+public class CalculatorTests {
+
+	@Test
+	public void testAdd() {
+	   int exp = 30;
+	   Calculator cal = new Calculator();
+	   int act = cal.add(10, 20);
+	   assertEquals(exp, act);
+	}
+
+}
